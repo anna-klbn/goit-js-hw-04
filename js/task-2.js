143534ffd41a4288dbb8d1314fface724ad53f15
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2025-09-06 20:12:55 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,33 +1,30 @@
-// Виправ помилки, які будуть в консолі, щоб скрипт запрацював.
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
 
-// ```js
-// const inventory = {
-//   items: ["Knife", "Gas mask"],
-//   add(itemName) {
-//     console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
 
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     console.log(`Removing ${itemName} from inventory`);
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
 
-//     this.items = this.items.filter((item) => item !== itemName);
-//   },
-// };
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
 
-// const invokeInventoryAction = function (itemName, action) {
-//   console.log(`Invoking action on ${itemName}`);
-//   action(itemName);
-// };
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
 
-// invokeInventoryAction("Medkit", inventory.add);
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
 
-// invokeInventoryAction("Gas mask", inventory.remove);
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
-
-// console.log(inventory.items); // ['Knife', 'Medkit']
+console.log(inventory.items); // ['Knife', 'Medkit']
